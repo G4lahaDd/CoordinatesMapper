dd3ca13f49e62deea37cd8c56b35b4cdd4690d23
Ilya <ilyakazyro@gmail.com>
2023-10-25 18:33:48 +0300

    Исправлена ошибка направляющих векторов

--- Mapper/Camera.cpp
+++ Mapper/Camera.cpp
@@ -1,14 +1,16 @@
 #include "Camera.h"
+#include <iostream>
+
 #define degToRad 3.1415/180
 
 Camera::Camera(Vector3 position, Vector3 rotation) : position(position), rotation(rotation)
 {
 	transform = Transform(position, rotation, Vector3(1,1,1));
 
-	double** matrix = transform.getMatrix();
-	ViewRay = Vector3(0, 1, 0) * matrix;
-	rightAxis = Vector3(1, 0, 0) * matrix;
-	upAxis = Vector3(0, 0, 1) * matrix;
+	double** matrix = Transform(Vector3(), rotation, Vector3(1, 1, 1)).getMatrix();
+	rightAxis = Vector3(1, 0, 0) * matrix; // i 
+	ViewRay = Vector3(0, 1, 0) * matrix; // j
+	upAxis = Vector3(0, 0, 1) * matrix; // k
 
 	height = 1080;
 	width = 1920;
@@ -26,7 +28,7 @@ Vector3 Camera::GetOnPlane(Vector2 point, Plane plane)
 		+ rightAxis * ((point.x - width / 2.0) * pixel2unitWidth)
 		+ upAxis * ((point.y - height / 2.0) * pixel2unitHeight);
 
-	point3d = plane.GetIntersection(position, point3d);
+	point3d = plane.GetIntersection(position, position + point3d);
 
 	return point3d;
 }
